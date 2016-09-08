5b6cfa90866a141d40669b26fc32a86bb4b5008d
Mike <miguel.8a.hdez@gmail.com>
2016-09-08 14:29:11 -0500

    added -s parameter to app's deploy

--- setup.sh
+++ setup.sh
@@ -1,15 +1,26 @@
 #/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
+echo "  ---> Removing .git dir"
 rm -fr .git
 
 # Creating app
+echo "  ---> Creating nexcloud instance"
 rhc app create nextcloud php-5.4 mysql-5.5 cron-1.4 -s
 
-rm nextcloud/index.php
-# Coping data to app
-rm -rf nextcloud/.openshift && mv .openshift nextcloud/
-mv php nextcloud/
-cd nextcloud
+echo "  ---> Getting repo"
+rhc git-clone nextcloud
 
+echo "  ---> Removing default php configurations"
+rm nextcloud/index.php
+rm -rf nextcloud/.openshift
+
+# Coping data to app
+echo "  ---> Copying base service"
+mkdir -p nextcloud/.openshift && mkdir -p nextcloud/php
+mv .openshift nextcloud/.openshift
+mv php nextcloud/php
+
+echo "  ---> Deploying nextcloud to openshift"
+cd nextcloud
 git add -A && git commit -m "init nextcloud" && git push
