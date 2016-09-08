bfdca40a276b47cfc0a15dd956091b7b50624ca9
Mike <miguel.8a.hdez@gmail.com>
2016-09-08 14:14:52 -0500

    auto setup test

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+# -*- ENCODING: UTF-8 -*-
+
+rm -fr .git
+
+# Creating app
+rhc app create nextcloud php-5.4 mysql-5.5 cron-1.4
+
+rm nextcloud/index.php
+# Coping data to app
+rm -rf nextcloud/.openshift && mv .openshift nextcloud/
+mv php nextcloud/
+cd nextcloud
+
+git add -A && git commit -m "init nextcloud" && git push
